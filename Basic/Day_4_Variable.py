cf63694dfc403071f1630c6ed5a5a446bb4efdcc
KhanIsrakAhmed <kahmed221275@bscse.uiu.ac.bd>
2025-03-09 23:40:55 +0600

    Update Day_4_Variable.py

--- Basic/Day_4_Variable.py
+++ Basic/Day_4_Variable.py
@@ -32,7 +32,9 @@ if age>18:
     print("You can vote")
     print("Your age : " + str(age))
 
-
+elif age<18 and age>6:
+    print("You are a Boy/Girl")
+    print("Your age : " + str(age))
 
 else:
     print("You are a Child")
