d5379fb76cc98c2518f5b6449ccc53c43c6c22de
KhanIsrakAhmed <kahmed221275@bscse.uiu.ac.bd>
2025-03-22 02:18:39 +0600

    Redo

---
+++ Basic/day_6_Module.py
@@ -0,0 +1,24 @@
+import math
+print(math.pi)
+print(math.sqrt(16))
+
+
+from math import * # try to avoide
+print(pi)
+print(sqrt(16))
+
+
+import math as m 
+print(m.pi)
+
+
+from math import pi,sqrt   # Good Practice LEVEL
+print(pi)
+
+from math import pi as PI
+print(PI)
+
+
+print(dir(math))
+
+print(dir())
